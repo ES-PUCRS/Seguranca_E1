5dd84b01e9d929547fe4416f283b725516938a1a
hcley <cleysonb.oliveira@gmail.com>
2022-09-29 18:08:17 -0300

    commit vigenere decode

--- run.bat
+++ run.bat
@@ -4,9 +4,9 @@ rem curl https://moodle.pucrs.br/mod/folder/view.php?id=2410676 -o textoscifrado
 
 SET "root=%~dp0"
 SET "binPath=%root%bin"
-if NOT exist %binPath% MKDIR %binPath%
+if NOT exist "%binPath%" MKDIR "%binPath%"
 
-javac src\*.java -d %binPath% -Xlint:unchecked -Xdiags:verbose 
+javac src\*.java -d "%binPath%" -Xlint:unchecked -Xdiags:verbose 
 
 if ["%ERRORLEVEL%"]==["0"] (
 	java -cp %binPath% app %1
